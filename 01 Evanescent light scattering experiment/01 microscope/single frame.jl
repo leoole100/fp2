e64b1ba0cf5f3f7af4f250641574611e512827f0
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-10-30 14:45:07 +0100

    add method using local minimum

--- 01 Evanescent light scattering experiment/01 microscope/single frame.jl
+++ 01 Evanescent light scattering experiment/01 microscope/single frame.jl
@@ -6,14 +6,15 @@ includet("functions.jl")
 # generate a test image
 img = fill(Gray(0.7), 200, 200)
 img .= clamp01.(img .+ 0.1*randn(200, 200))
-draw_circle!(img, Point(150, 150), 40)
-draw_circle!(img, Point(100, 30), 10)
+draw_circle!(img, Point(150, 150), 30)
+draw_circle!(img, Point(100, 30), 20)
 draw_line!(img, Point(20, 20), Point(170, 170))
+img .= clamp01.(img .+ 0.1*randn(200, 200))
 
 img
 
 #%%
-# find circles in the image
+# find circles in the image using the Hough transform
 centers, radii = find_circles(img, 7:60;
 	min_dist=20,
 	vote_threshold=25,
@@ -26,7 +27,7 @@ end
 particles
 
 # %%
-# crop the image for the particles
+# optimize using the center of mass
 padding = 1
 
 x_coords = [x for y in 1:size(img, 1), x in 1:size(img, 2)]
@@ -38,21 +39,39 @@ particles.img = Vector(undef, nrow(particles))
 
 for p in eachrow(particles)
 	img_cropped = crop_image(img, p.c, p.r, padding)
-	c = center_of_mass((1 .- img_cropped), x_coords, y_coords, p.c, p.r, padding)
+	c = center_of_mass((1 .- img_cropped).^2, x_coords, y_coords, p.c, p.r, padding)
 	p.img = img_cropped
 	p.c_opt = c
 end
 
 particles
 
-#%%
+
+# %%
+# using local minimum search
+# blur the image
+img_blurred = imfilter(img, Kernel.gaussian(20))
+1 .- img_blurred
+minima = findlocalminima(img_blurred)
+minima = DataFrame(c = [(c[2], c[1]) for c in minima])
+minima.strength = [Float64(img_blurred[c[2], c[1]]) for c in minima.c]
+sort!(minima, :strength, rev=false)
+minima = minima[ minima.strength .< 0.5, :]
+
+minima
+
+# %%
+# plot the results
 f = Figure()
 
 image(f[1, 1], img,
 	axis = (aspect = DataAspect(), yreversed = true,)
 )
 
-scatter!(f[1, 1], last.(particles.c), first.(particles.c), color = :red)
-scatter!(f[1, 1], last.(particles.c_opt), first.(particles.c_opt), color = :yellow)
+scatter!(f[1, 1], last.(particles.c), first.(particles.c), color = :red, label = "Hough transform")
+scatter!(f[1, 1], last.(particles.c_opt), first.(particles.c_opt), color = :yellow, label = "Center of mass")
+scatter!(f[1, 1], last.(minima.c), first.(minima.c), color = :green, label = "Local minima")
+
+axislegend()
 
 f
