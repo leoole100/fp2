edb94eb7b23577185b6ef34711de51be986311e7
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-03 00:08:03 +0100

    added exp fit

--- 01 Evanescent light scattering experiment/01 microscope/02 path.jl
+++ 01 Evanescent light scattering experiment/01 microscope/02 path.jl
@@ -18,10 +18,12 @@ trajectory = []
 for i in 1:ncol(f)÷2
 	push!(df[!, :trajectory], hcat(f[:,2*i-1], f[:,2*i]))
 end
+df[!, :frames] = [1:size(p.trajectory)[1] for p in eachrow(df)]
 
 df
 
-
+# %%
+# plot the trajectories on the image
 f = Figure()
 image(f[1,1], img', axis=(aspect=DataAspect(), yreversed=true,), interpolate=false)
 for p in eachrow(df)
@@ -31,20 +33,35 @@ f
 
 # %%
 # calculate the mean squared displacement
-model(x, p) = clamp.(p[2] .* x, 0, p[3])
 
 # df[!, :reference] = [mean(p.trajectory, dims=1) for p in eachrow(df)]
 df[!, :reference] = [p.trajectory[1, :] for p in eachrow(df)]
-df[!, :msd] = [[sum((p.trajectory[j,:] .- p.reference).^2) for j in 1:size(p.trajectory,1)] for p in eachrow(df)]
-df[!, :fit] = [LsqFit.curve_fit(model, 1:size(p.trajectory,1), p.msd, [0.0, 0.0, 1.0]) for p in eachrow(df)]
+df[!, :msd] = [[sum((p.trajectory[j,:] .- p.reference).^2) for j in p.frames] for p in eachrow(df)]
+
+# %%
+
+linear_model(x, p) = clamp.(p[2] .* x, 0, p[3])
+df[!, :linear_model] = [curve_fit(linear_model, p.frames, p.msd, [0.0, 0.0, 1.0]) for p in eachrow(df)]
+
+exp_model(x, p) = p[1] .- p[1] .* exp.(-p[2] .* x ./ p[1])
+df[!, :exp_model] = [curve_fit(exp_model, p.frames, p.msd, [50.,1.]) for p in eachrow(df)]
+
+#%%
+
 
 f = Figure()
 a =Axis(f[1, 1])
 for i in eachrow(df)
 	plot!(a, 1:size(i.trajectory,1), i.msd)
-	if i.fit.converged
-		lines!(a, 1:size(i.trajectory,1), model(1:size(i.trajectory,1), i.fit.param))
+	if i.linear_model.converged
+		# lines!(a, 1:size(i.trajectory,1), linear_model(1:size(i.trajectory,1), i.linear_model.param))
+	end
+	if i.exp_model.converged
+		lines!(a, 1:size(i.trajectory,1), exp_model(1:size(i.trajectory,1), i.exp_model.param))
 	end
 end
+lines!(0:100, x->linear_model(x, [0, 100., 1000.]), label="linear")
+lines!(0:100, x->exp_model(x, [1000., 100.]), label="exp")
+axislegend(a)
 
 f
