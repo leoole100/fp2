beb070654368752bb481a6d0b4e73c44c34aa629
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-03 01:09:24 +0100

    added potential analysis

--- 01 Evanescent light scattering experiment/01 microscope/02 path.jl
+++ 01 Evanescent light scattering experiment/01 microscope/02 path.jl
@@ -1,6 +1,6 @@
 # find particles in images and track them
 
-using DataFrames, Statistics, LsqFit
+using DataFrames, Statistics, LsqFit, LinearAlgebra
 using WGLMakie
 import CSV, Images
 
@@ -27,7 +27,9 @@ df
 f = Figure()
 image(f[1,1], img', axis=(aspect=DataAspect(), yreversed=true,), interpolate=false)
 for p in eachrow(df)
-	scatter!(f[1,1], p.trajectory)
+	scatter!( p.trajectory)
+	# points = [Point2f(p.trajectory[j,:]...) for j in p.frames]
+	# datashader!(points)
 end
 f
 
@@ -35,22 +37,18 @@ f
 # calculate the mean squared displacement
 
 # df[!, :reference] = [mean(p.trajectory, dims=1) for p in eachrow(df)]
-df[!, :reference] = [p.trajectory[1, :] for p in eachrow(df)]
-df[!, :msd] = [[sum((p.trajectory[j,:] .- p.reference).^2) for j in p.frames] for p in eachrow(df)]
+df[!, :reference] = [p.trajectory[1, :] for p in eachrow(df)];
+df[!, :msd] = [[sum((p.trajectory[j,:] .- p.reference).^2) for j in p.frames] for p in eachrow(df)];
 
-# %%
+linear_model(x, p) = clamp.(p[2] .* x, 0, p[3]);
+df[!, :linear_model] = [curve_fit(linear_model, p.frames, p.msd, [0.0, 0.0, 1.0]) for p in eachrow(df)];
 
-linear_model(x, p) = clamp.(p[2] .* x, 0, p[3])
-df[!, :linear_model] = [curve_fit(linear_model, p.frames, p.msd, [0.0, 0.0, 1.0]) for p in eachrow(df)]
-
-exp_model(x, p) = p[1] .- p[1] .* exp.(-p[2] .* x ./ p[1])
-df[!, :exp_model] = [curve_fit(exp_model, p.frames, p.msd, [50.,1.]) for p in eachrow(df)]
-
-#%%
+exp_model(x, p) = p[1] .- p[1] .* exp.(-p[2] .* x ./ p[1]);
+df[!, :exp_model] = [curve_fit(exp_model, p.frames, p.msd, [50.,1.]) for p in eachrow(df)];
 
 
 f = Figure()
-a =Axis(f[1, 1])
+a = Axis(f[1, 1], xlabel="time", ylabel="mean squared displacement")
 for i in eachrow(df)
 	plot!(a, 1:size(i.trajectory,1), i.msd)
 	if i.linear_model.converged
@@ -60,8 +58,27 @@ for i in eachrow(df)
 		lines!(a, 1:size(i.trajectory,1), exp_model(1:size(i.trajectory,1), i.exp_model.param))
 	end
 end
+# try models
 lines!(0:100, x->linear_model(x, [0, 100., 1000.]), label="linear")
 lines!(0:100, x->exp_model(x, [1000., 100.]), label="exp")
 axislegend(a)
-
 f
+
+# %%
+# calculate the probability distribution of the displacements
+df[!, :center] = [mean(p.trajectory, dims=1) for p in eachrow(df)]
+df[!, :distance] = [[norm(p.trajectory[j,:]' .- p.center) for j in p.frames] for p in eachrow(df)]
+df[!, :σ] = [std(p.distance) for p in eachrow(df)]
+
+f = Figure()
+a = Axis(f[1, 1], xlabel="distance from center", ylabel="probability")
+for i in 1:nrow(df)
+	p = df[i, :]
+	offset = i
+	hist!(p.distance, normalization=:probability, offset=offset, scale_to=0.6, )
+	lines!(a, 0:0.1:maximum(p.distance), x->exp(-x^2/(2*p.σ^2))*.6 +offset,)
+end
+f
+
+# %%
+# calculate the probability distribution for a given potential
