d810f54b84771b8e5a8a75770d400aa5a95a4934
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-04 17:26:11 +0100

    made nicer path algorithm

--- 01 Evanescent light scattering experiment/01 microscope/02 path.jl
+++ 01 Evanescent light scattering experiment/01 microscope/02 path.jl
@@ -1,62 +1,63 @@
 # find particles in images and track them
 
-using DataFrames, Statistics, LsqFit, LinearAlgebra
-using CairoMakie
+# using DataFrames, Statistics, LsqFit, LinearAlgebra
+using DataFrames: DataFrame, nrow, eachrow, ncol
+using Statistics: mean, std
+using LsqFit: curve_fit
+using LinearAlgebra: norm, normalize
+using WGLMakie
 import CSV, Images
+using StatsBase: fit, Histogram
+using KernelDensity: kde
+
+# %%
 
 path = "../data/test/particles"
 
-# %%
 # open the trajectory
 cd(@__DIR__)
 img = Images.load(path * ".mean.png");
 f = DataFrame(CSV.File(path * ".trajectory.csv"))
 
-df = DataFrame(trajectory=[])
+df = DataFrame(t=[], n=[])
 
-trajectory = []
 for i in 1:ncol(f)÷2
-	push!(df[!, :trajectory], hcat(f[:,2*i-1], f[:,2*i]))
+	push!(df[!, :t], hcat(f[:,2*i-1], f[:,2*i]))
+	push!(df[!, :n], split(path, "/")[end] * " $i")
 end
-df[!, :frames] = [1:size(p.trajectory)[1] for p in eachrow(df)]
-
 df
 
-# %%
 # plot the trajectories on the image
 f = Figure()
 image(f[1,1], img', axis=(aspect=DataAspect(), yreversed=true,), interpolate=false)
 for p in eachrow(df)
-	scatter!( p.trajectory)
+	scatter!(p.t, label=p.n)
 	# points = [Point2f(p.trajectory[j,:]...) for j in p.frames]
 	# datashader!(points)
 end
+axislegend()
 f
 
 # %%
 # calculate the mean squared displacement
 
-# df[!, :reference] = [mean(p.trajectory, dims=1) for p in eachrow(df)]
-df[!, :reference] = [p.trajectory[1, :] for p in eachrow(df)];
-df[!, :msd] = [[sum((p.trajectory[j,:] .- p.reference).^2) for j in p.frames] for p in eachrow(df)];
+reference(t) = t[1,:]
+msd(t, r=reference(t)) = sum((t .- r').^2, dims=2)[:,1]
+msd(t) = msd(t, reference(t))
+msd(df[1,:t])
 
-linear_model(x, p) = clamp.(p[2] .* x, 0, p[3]);
-df[!, :linear_model] = [curve_fit(linear_model, p.frames, p.msd, [0.0, 0.0, 1.0]) for p in eachrow(df)];
-
+linear_model(x, p) = clamp.(p[1] .* x, 0, p[2]);
 exp_model(x, p) = p[1] .- p[1] .* exp.(-p[2] .* x ./ p[1]);
-df[!, :exp_model] = [curve_fit(exp_model, p.frames, p.msd, [50.,1.]) for p in eachrow(df)];
-
-#%%
+fit_linear_model(t, times=1:size(t,1), p0=[1.0, 1.0]) = curve_fit(linear_model, times, msd(t), p0)
+fit_exp_model(t, times=1:size(t,1), p0=[1000., 100.]) = curve_fit(exp_model, times, msd(t), p0)
 
 f = Figure();
 a = Axis(f[1, 1], xlabel="time", ylabel="mean squared displacement");
 for i in eachrow(df)
-	plot!(a, 1:size(i.trajectory,1), i.msd)
-	if i.linear_model.converged
-		# lines!(a, 1:size(i.trajectory,1), linear_model(1:size(i.trajectory,1), i.linear_model.param))
-	end
-	if i.exp_model.converged
-		lines!(a, 1:size(i.trajectory,1), exp_model(1:size(i.trajectory,1), i.exp_model.param))
+	scatter!(msd(i.t), label=i.n)
+	for (m, l) in zip([fit_linear_model, fit_exp_model], ["linear", "exp"])
+		p = m(i.t)
+		lines!(0:100, x->linear_model(x, p.param))
 	end
 end
 # try models
@@ -67,38 +68,30 @@ f
 
 # %%
 # calculate the probability distribution of the displacements
-df[!, :center] = [mean(p.trajectory, dims=1) for p in eachrow(df)]
-df[!, :distance] = [[norm(p.trajectory[j,:]' .- p.center) for j in p.frames] for p in eachrow(df)]
-df[!, :σ] = [std(p.distance) for p in eachrow(df)]
+center(t) = mean(t, dims=1)
+radius(t, c=center(t)) = [norm(t[j,:]' .- c) for j in 1:size(t,1)]
+σ(t) = std(radius(t))
+σ(df[1,:t]) # = dV/dx^2 / kT
+
+potential(p) = -log.(p)
+histogram(data) = normalize(fit(Histogram, data), mode=:probability)
 
 f = Figure()
-a = Axis(f[1, 1], xlabel="distance from center", ylabel="probability")
-for i in 1:nrow(df)
-	p = df[i, :]
-	offset = i
-	hist!(p.distance, normalization=:probability, offset=offset, scale_to=0.6, )
-	lines!(a, 0:0.1:maximum(p.distance), x->exp(-x^2/(2*p.σ^2))*.6 +offset,)
-end
-f
+a = Axis(f[1, 1], ylabel="probability")
+b = Axis(f[2, 1], xlabel="distance from center", ylabel="Potential in kT")
+linkxaxes!(a, b)
+hidexdecorations!(a, ticks = false)
+for i in eachrow(df)
+	r = radius(i.t)
+	h = histogram(r)
+	k = kde(r, boundary=(minimum(r), maximum(r)))
 
-# %%
-# calculate the probability distribution for a given potential
-using StatsBase
-function histogram(data)
-	h = fit(Histogram, data)
-	x = h.edges[1][1:end-1] .+ diff(h.edges[1]) ./ 2
-	y = h.weights
-	return [x, y]
-end
-df[!, :histogram] = [histogram(p.distance) for p in eachrow(df)]
-potential(hist) = [hist[1], -log.(hist[2])]
-df[!, :potential] = [potential(p.histogram) for p in eachrow(df)]
+	l = stairs!(a, h.edges[1][1:end-1], h.weights, linestyle=:dash)
+	scatter!(b, h.edges[1][1:end-1], potential(h.weights), color=l.color)
 
-f = Figure()
-a = Axis(f[1, 1], xlabel="distance from center", ylabel="potential in kT")
-for i in 1:nrow(df)
-	p = df[i, :]
-	offset = i
-	plot!(a, p.potential...)
+	lines!(a, k.x, k.density, color=l.color)
+	lines!(b, k.x, potential(k.density), color=l.color, label=i.n)
 end
+axislegend()
+save("../figures/01_potential.png", f)
 f
