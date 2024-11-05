a9c720e07bc35ad494ed8bbb495b761f75603004
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-05 13:49:36 +0100

    added gravitational model

--- 01 Evanescent light scattering experiment/02 TIRM/potential.jl
+++ 01 Evanescent light scattering experiment/02 TIRM/potential.jl
@@ -40,20 +40,23 @@ background(I) = 0
 estimate_I0(I) = maximum(I)-background(I)
 calc_z(I, I0=estimate_I0(I), beta=1, b=background(I)) = log.(I0 ./ (I.-b)) ./ beta
 potential(p) = -log.(p) .+ log(maximum(p))
-function dist(data; cutoff=-Inf)
+function dist(data; cutoff=0)
 	k = kde(data, boundary=(minimum(data), maximum(data)))
 	x, y = k.x, k.density
 	x, y = x[y .> cutoff], y[y .> cutoff]
 	return (x=x, y=y)
 end
 
+model(x, p) = p[1] .* exp.(-x .* p[2]) .+ p[3] .* x .- p[4]
+fit_model(x, y) = curve_fit(model, x, y, [10, 1.0, 1.0, 1.0])
+
 f = Figure()
 aV = Axis(f[1, 1], ylabel="V in kT")
-az = Axis(f[2, 1], ylabel="pdf", xlabel="z")
+# az = Axis(f[2, 1], ylabel="pdf", xlabel="z")
 # aI = Axis(f[2, 2], xlabel="intensity")
-linkxaxes!(az, aV)
+# linkxaxes!(az, aV)
 # linkyaxes!(aI, az)
-ylims!(az, 0, nothing)
+# ylims!(az, 0, nothing)
 # hidexdecorations!(aV, grid=false)
 # hideydecorations!(aI, grid=false)
 for i in eachrow(df)
@@ -61,9 +64,12 @@ for i in eachrow(df)
 	# lines!(aI, pI.x, pI.y, label=i.l)
 	z = calc_z(i.I)
 	pZ = dist(z; cutoff=0.01)
-	lines!(az, pZ.x, pZ.y, label=i.l)
+	# lines!(az, pZ.x, pZ.y, label=i.l)
 	v = potential(pZ.y)
 	l = lines!(aV, pZ.x, v, label=i.l)
+	fit = fit_model(pZ.x, v)
+	lines!(aV, pZ.x, model(pZ.x, fit.param), color=l.color, linestyle=:dash)
 end
+# lines!(aV, 0:.1:8, x -> model(x, [10, 1.0, 1.0, 1.0]), color=:black)
 axislegend(aV)
 f
