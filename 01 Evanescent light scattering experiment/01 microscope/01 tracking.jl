89a89595f6e2307ad15c9e0c1953b01c5afb7e9a
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-09 10:24:16 +0100

    tracked particles

--- 01 Evanescent light scattering experiment/01 microscope/01 tracking.jl
+++ 01 Evanescent light scattering experiment/01 microscope/01 tracking.jl
@@ -7,8 +7,9 @@ using Colors: Gray
 using Images: clamp01
 using WGLMakie
 import CSV, VideoIO
+using FileIO: load
 
-path = "../data/test/particles.avi"
+path = "../data/TLM/006 pt02 ot0.avi"
 
 #%%
 # open video file
@@ -18,10 +19,11 @@ img = read(video)
 # image(img',axis=(aspect=DataAspect(),yreversed = true,))
 
 # make normalization images
-white = fill(maximum(Gray{Float64}.(img)), size(img))
-black = fill(minimum(Gray{Float64}.(img)), size(img))
+white = load("../data/TLM/white.bmp")
+black = load("../data/TLM/black.bmp")
+white, black = float.(Gray.(white)), float.(Gray.(black))
 
-normalize(img) = (clamp01.(Gray.(img)) .- black) ./ (white .- black)
+normalize(img) = (clamp01.(Gray.(float(img))) .- black) ./ (white .- black)
 normalize(img)
 
 # transform the images
@@ -29,33 +31,18 @@ trans(x) = x^4
 transform(img) = Gray.(trans.(1 .- normalize(img)))
 transform(img)
 
-image(transform(img)', axis=(aspect=DataAspect(), yreversed=true,), interpolate=false)
+image(transform(img)'./maximum(transform(img)), axis=(aspect=DataAspect(), yreversed=true,), interpolate=false)
+
 
 # %%
-# find particles in the images using Laplacian of Gaussian blob detection
-blobs = blob_LoG(transform(img),  range(2,10, length=10));
-blobs = DataFrame(
-	c = [(blob.location[2], blob.location[1]) for blob in blobs],
-	σ = [blob.σ[1] for blob in blobs],
-	a = [blob.amplitude for blob in blobs],
-);
-sort!(blobs, :a, rev=true)
-# blobs = blobs[ blobs.a .> 0.03, :]
-blobs = blobs[1:2, :]
-blobs.r = blobs.σ .* 2;
-# blobs
-
-image(img', axis=(aspect=DataAspect(), yreversed=true,), interpolate=false)
-scatter!(blobs.c, markersize=blobs.r.*2, color=:red, marker=:x, markerspace=:data)
-current_figure()
-
-# %%
-# track the particles
-last_positions = hcat([Float64.([first(i), last(i)]) for i in blobs.c]...)'
-radii = blobs.r
 
 coords = [Float64.([x, y ]) for y in 1:size(img, 1), x in 1:size(img, 2)];
 com(i, c) = sum(c .* Float64.(i))/sum(Float64.(i)) # center of mass
+
+# find single particle
+last_positions = hcat(com(transform(img), coords))'
+radii = [40]
+
 function crop(img, c, r)
 	x, y = round.(Int, c)
 	r = round(Int, r)
@@ -63,9 +50,10 @@ function crop(img, c, r)
 	y1, y2 = max(1, y-r), min(size(img, 1), y+r)
 	return @view img[y1:y2, x1:x2]	
 end
-# crop(transform(img), last_positions[1, :], radii[1])
 cropped_com(img, c, r) = com(crop(img, c, r), crop(coords, c, r))
 # cropped_com(transform(img), last_positions[1, :], radii[1])
+
+# @time com(transform(img), coords)
 
 function update_positions!(last_positions, radii, img)
 	for i in 1:length(radii)
@@ -73,12 +61,15 @@ function update_positions!(last_positions, radii, img)
 	end
 end
 
-# @time update_positions!(last_positions, radii, transform(img)) # <1 ms
+update_positions!(last_positions, radii, transform(img))
+@time update_positions!(last_positions, radii, transform(img)) # <1 ms
 
+crop(normalize(img), last_positions[1, :], radii[1]) # show the particle
 
 # %%
 # track the particles
-last_positions = transpose(hcat([Float64.([first(i), last(i)]) for i in blobs.c]...))
+# last_positions = transpose(hcat([Float64.([first(i), last(i)]) for i in blobs.c]...))
+last_positions = hcat(com(transform(img), coords))'
 last_positions = reinterpret(Float64, last_positions)
 positions = Array{Float64, 2}[last_positions]
 
@@ -86,31 +77,32 @@ video = VideoIO.openvideo(path)
 img = read(video)
 transformed = transform(img)
 meanImg = Gray{Float64}.(transformed)
+i = 0
 while !eof(video)
 	img = read(video)
 	transformed = transform(img)
 	update_positions!(last_positions, radii, transformed)
 	push!(positions, last_positions)
-	meanImg = meanImg .+ img
+	# meanImg = normalize(meanImg) .+ img
+	i += 1
+	if i % 100 == 0
+		println(i)
+	end
 end
 
-meanImg = meanImg ./ length(positions)
+# meanImg = meanImg ./ length(positions)
 positions
 
 # %%
 df = DataFrame()
 
 # add a column for each particle
-for i in 1:2
-	df[!, "x$i"] = [pos[i, 1] for pos in positions]
-	df[!, "y$i"] = [pos[i, 2] for pos in positions]
-end
+df[!, "x"] = [p[1] for p in positions]
+df[!, "y"] = [p[2] for p in positions]
 
 df
 
 basepath = join(split(path, '.')[1:end-1], ".")
 CSV.write(basepath * ".trajectory.csv", df)
-save(basepath * ".mean.png", clamp01.(Gray.(meanImg)))
-for i in 1:length(radii)
-	save(basepath * ".particle$i.png", crop(img, last_positions[i, :], radii[i] .+ 5))
-end
+save(basepath * ".png", clamp01.(Gray.(img)))
+save(basepath * ".particle.png", clamp01.(crop(normalize(img), last_positions[1, :], radii[1])))
