4d2161890ab22ce6053e2b86c9e586df4d46fbeb
Aurel <aurel.mueller-schoenau@uni-konstanz.de>
2024-11-13 00:18:03 +0100

    julia mag Optim nicht =(

--- 01 Evanescent light scattering experiment/02 TIRM/teil_2.jl
+++ 01 Evanescent light scattering experiment/02 TIRM/teil_2.jl
@@ -3,14 +3,40 @@
 
 using Roots: fzero
 using ForwardDiff: derivative
-using WGLMakie
+using CairoMakie
+using Optim: optimize, LBFGS
+using LsqFit: curve_fit
+using Glob: glob
+using DataFrames: DataFrame
+using KernelDensity: kde
+import CSV, Images
+import Optim
+include("../functions.jl")
 
 cd(@__DIR__)
 
 # %%
-k_B = 1
-eta = 1
-beta = 1
+k_B = 1.38e-23
+eta = 1.0019e-3
+beta = 1/10e-9
+TIMESTEP = 1e-3
+
+function read_data()
+    path = "../data/TIRM/"
+    paths = glob(path * "*/*.dat")
+    paths = filter(p -> occursin("failed", lowercase(p)) == false, paths)
+    Ia = []
+    pa = []
+    for p in paths
+        I = DataFrame(CSV.File(p, header=["t", "I"])).I
+        push!(Ia, I)
+    push!(pa, split(p, "/")[end-1])
+    end
+    df = DataFrame(I=Ia, p=pa)
+    df.l = map(p -> split(p, " ")[3]*" "*split(p, " ")[2], df[:,:p])
+    df.ot = map(p -> parse(Float64, split(p, " ")[3][3:end]), df[:,:p])
+    df
+end
 
 function calc_D_0(R,T)
     k_B*T/(6*pi*eta*R)
@@ -37,7 +63,7 @@ function sigmaq(delta_z)
 end
 
 function calc_D_z(sigmaq,delta_t)
-    sigmaq/(2*delta_t)
+    sigmaq/(2*delta_t*TIMESTEP)
 end 
 
 function estimate_I0(I_z,guessI0,delta_t,R,T)
@@ -45,15 +71,71 @@ function estimate_I0(I_z,guessI0,delta_t,R,T)
     t_steps = [n*delta_t for n in range(1,round(Int,(length(I_z)-1)/delta_t)-1)]
     err_func(I_0) = sum((calc_D_z.(sigmaq.(slice_dz.(t_steps,delta_t,Ref(delta_z(find_z.(beta,I_z,I_0))))),delta_t).-D_orth.(D_0,R,sum.(slice_dz.(t_steps,delta_t,Ref(find_z.(beta,I_z,I_0))))./delta_t)).^2)
     D(f)=x -> derivative(err_func,float(x))
-    fzero(D(err_func), guessI0)
+    I_0 = fzero(D(err_func), guessI0)
+    println(err_func(I_0))
+    I_0
 end
+#%%
 
+data = read_data()
+#%%
+#data.I[1]
+data.ot
 
-I_z = exp.(beta * randn(Integer(1e6)))
+#%%
+I_z = data.I[1]
+#I_z = exp.(beta * randn(Integer(1e6)))
 guessI0 = 10
-delta_t = 10
-R = 1
-T = 100
+delta_t = 1000
+R = 1.86e-6
+T = 273+22
+
+#%%
+estimate_I0(I_z,guessI0, delta_t, R, T)
+
+#%%
+D_0 = calc_D_0(R,T)
+t_steps = [n*delta_t for n in range(1,round(Int,(length(I_z)-1)/delta_t)-1)]
+model(p) = sum((calc_D_z.(sigmaq.(slice_dz.(t_steps,delta_t,Ref(delta_z(find_z.(p[1],I_z,p[2]))))),delta_t).-D_orth.(D_0,R,sum.(slice_dz.(t_steps,delta_t,Ref(find_z.(p[1],I_z,p[2]))))./delta_t)).^2)
+#%%
+
+function hist(traj,vrange)
+    traj_hist = zeros(length(vrange))
+    traj_hist[1] = sum(traj[traj.<vrange[1]])
+    for i in range(2,length(vrange))
+        traj_hist[i] = sum(traj[vrange[i-1].<traj.<=vrange[i]])
+    end
+    traj_hist/length(traj)
+end
+(vrange,trahi) = dist(I_z)
+model(v,p) = p[4]/(sqrt(2*pi)*p[1]).*exp.(-p[3]^2*(log.(abs.(1 .-v)).-log.(abs(p[2]))).^2 ./(2*p[1]^2))
+lb = [1e-9,minimum(I_z),10e-9,0]
+ub = [1e-7,maximum(I_z),1e-1,10000]
+p0 = [2.35e-8,0.53,1e-7,1.83e-7]
+#pf = curve_fit(model,vrange,trahi,p0,g_tol=0,x_tol=0,show_trace=true,iterations=10000)#,upper=ub,lower=lb)
+function err_func(p)
+    sum((model(vrange,p).-trahi).^2)
+end
+pf = optimize(err_func,p0,LBFGS(),Optim.Options(iterations=10000))
+f = Figure()
+i = Axis(f[1,1])
+#i2 = Axis(f[2,1])
+lines!(vrange,trahi,label="trahi")
+lines!(vrange,model(vrange,p0),label="p0")
+if pf.converged
+    lines!(vrange,model(vrange,pf.param),label="pf",linestyle=:dot)
+end
+axislegend()
+f
+
+#%%
+f = Figure()
+i = Axis(f[1,1], ylabel="intensity")
+ih = Axis(f[1,2])
+lines!(i, I_z)
+density!(ih, I_z, direction=:y)
+f
+#%%
 
 # I0 = estimate_I0(I_z,guessI0,delta_t,R,T)
 I0 = maximum(I_z)
