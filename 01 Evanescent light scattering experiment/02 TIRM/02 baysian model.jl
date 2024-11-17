81a14be1f9304ae1c94ea957a575ebe5793a592b
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-17 09:27:43 +0100

    added some random thoughts on a baysian model

--- 01 Evanescent light scattering experiment/02 TIRM/02 baysian model.jl
+++ 01 Evanescent light scattering experiment/02 TIRM/02 baysian model.jl
@@ -4,42 +4,49 @@
 # Δz = N(0, σ^2)
 
 using Turing
+using CairoMakie
+using Distributions
+using KernelDensity
+using StatsBase
+
+# %% generate test data
+I0 = 1
+β = 100
+sigma = .1
+z0 = 0.1
+
+
+dz = rand(Normal(z0, sigma), 1000)
+z = cumsum(dz)
+I = I0 .* exp.(-z ./ β)
+round_to_number(x, n) = round.(Int, x ./ n) .* n
+# I = round_to_number(I, .5)
+
+f = Figure()
+a = Axis(f[1, 1])
+density!(I)
+ylims!(low=0)
+f
 
 # %% define a model
-@model function model(I)
-	# Priors for the parameters
-	I0 ~ Truncated(Normal(0, 10), 0, Inf)
-	beta ~ LogNormal(0, 1)
-	σdz ~ truncated(Normal(0, 1), 0, Inf)
+@model function model(I_dist)
+	σ ~ truncated(Normal(0, 1), 0, Inf)
+	β ~ truncated(Normal(1, 1), 0, Inf)
+	I0 ~ truncated(Normal(10, 1), 0, Inf)
+	dZ ~ Normal(0, σ)
 
-	dZ = Normal(0, σdz)
-
-	
-
-	for i in eachindex(I)
-		I[i] 
-	end
-
-	# apply a random walk model
-	# z = Vector{Real}(undef, length(I))
-	# z[1] = 0
-	# for i in 2:length(I)
-	# 	z[i] ~ z[i-1] + Normal(0, σdz)
-	# end
-
-	# # apply the model
-	# for i in eachindex(I)
-	# 	I[i] ~ Normal(I0 * exp(-z[i] / beta), .1)
-	# end
+	Z ~ truncated(Normal(1, 1),0, Inf)
+	I_mdl = I0 .* exp.(-Z ./ β)
+	I_mdl ~ I_dist
 end
 
+I_dist = fit(Normal, I)
+k = kde(I)
+I_dist = pdf(k)
+kde_model
 
-# test the model
-dz = rand(Normal(5, .5), 100)
-z = cumsum(dz)
-I = 10 .* exp.(-z ./ 1.5)
+chain = sample(model(I_dist), NUTS(), 1000)
 
-chain = sample(model(I), NUTS(), 100)
 
-# %%
-chain
+# %% plot the sampled density
+
