7f6b38fa17a448c62ffa2ac623118b1bea005368
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-11-18 16:48:00 +0100

    made overview of measured diffusion coefficients

--- 01 Evanescent light scattering experiment/02 TIRM/04 histogram.jl
+++ 01 Evanescent light scattering experiment/02 TIRM/04 histogram.jl
@@ -1,12 +1,13 @@
 using CairoMakie
 using StatsBase: moment, std, mean, var
 using KernelDensity: kde
+using Optim
+using Format: format
 
 # load and filter data
 include("functions.jl")
 
 diff_z(z, d) = circshift(z, -d) .- z
-mean_slope(y) = mean(diff(y))
 
 # similar to the provided matlab code
 function dvdt(z; z_bins=100, t_bins=5, cutoff=1/100)
@@ -27,14 +28,18 @@ function dvdt(z; z_bins=100, t_bins=5, cutoff=1/100)
 	end
 	return z0, dzdt, count
 end
-function D(dσdt)
+
+function D(dσdt; mask=true)
 	z, v, c = dσdt
 	D = v ./ (2*Δt)
-	mask = z .< .4
-	return z[mask], D[mask], c[mask]
+	if mask
+		mask = z .< 0.4 .&& isnan.(D) .== false
+		return z[mask], D[mask], c[mask]
+	end
+	return z, D, c
 end
 
-# %% look at same z
+# %% look at dz distribution
 f = Figure()
 a = Axis(f[1,1],
 xlabel="dz", ylabel="pdf"
@@ -49,36 +54,68 @@ for i in eachrow(df)
 end
 f
 
+# %% implement optimization procedure
+function error(p; I)
+	z = z_estimate(I, I0=p[1], β=p[2])
+	z, d, c = D(dvdt(z, cutoff=1/100), mask=false)
+	theo = Dz_theoretical.(clamp.(z, 0, Inf))
+	d = d .* c
+	d = filter(!isnan, d)
+	if length(d) == 0
+		return Inf
+	end
+	return sum(d.^2)
+end
+
+function fit(I)
+	result = optimize(p->error(p, I=I), [1.2, .2])
+	return result
+end
+
+result = fit(df.I[4])
+result.minimizer
+
+
 # %%
+df[:, :fit] .= fill([1, .2], size(df, 1))
+l_beta = Dict(5.0=>.454, 10.0=>.322, 15.0=>.265, 20.0=>.230)
 
-p = (I0=1.2, β=.156)
-# p = (I0=.85, β=.016) #fits but only on linear part
-# p = (I0=2.5, β=0.3) # expected
+# add beta from df.l column
+df[:, :β] = zeros(size(df, 1))
+for i in 1:size(df, 1)
+	df[i, :β] = l_beta[df[i, :l]+5.0]
+	df[i, :fit] = [1.2, df[i, :β]]
+end
 
-f = Figure()
+df[4, :fit] = [1.3, .2]
+df[5, :fit] = [.9, .27]
+
+f = Figure(size=fullsize)
 a = Axis(f[1,1], 
 	xlabel="z in μm", ylabel="D(z) in μm²/s",
 	# yscale=log10,
 	# xscale=log10,
-	title="I₀="*string(p.I0)*", β="*string(p.β)*" μm"
 )
 lines!(
-	0:.01:.7, Dz_theoretical,
+	0:.01:.5, Dz_theoretical,
 	color=:black,
 	label="Theoretical",
 )
-for i in eachrow(df[4:4, :])
+for (i,r) in enumerate(eachrow(df))
 # for i in eachrow(df)
-	z = z_estimate(i.I, I0=p.I0, β=p.β)
-	z, d, c = D(dvdt(z, cutoff=1/50))
-	scatter!(z, d, 
-		color=i.ot, colorrange=extrema(df.ot),
-		label=i.p,
-		markersize=10 .*c./maximum(c).+2,
-		alpha=.7
+	# z = z_estimate(i.I, I0=p[1], β=p[2])
+	z = z_estimate(r.I, I0=r.fit[1], β=r.fit[2])
+	z, d, c = D(dvdt(z, cutoff=0))
+	scatter!(z, d,
+		color=r.ot, colorrange=extrema(df.ot),
+		label="Trap Strength: "*format(r.ot, precision=2)*" β_set="*format(r.β)*"\n"*"I₀="*string(r.fit[1])*", β_exp="*string(r.fit[2])*" μm",
+		markersize=7 .*c./maximum(c) .+ 3,
+		alpha=.7,
+		marker=[:circle, :rect, :diamond][mod(i, 3)+1]
 	)
 end
-# Legend(f[1,2], a, label="Trap Strength", framevisible=false)
-axislegend(position=:lt)
+Legend(f[1,2], a, framevisible=false)
 colgap!(f.layout, 1)
+resize_to_layout!(f)
+save("../figures/02_04_01_diffusion.pdf", f)
 f
--- 01 Evanescent light scattering experiment/02 TIRM/functions.jl
+++ 01 Evanescent light scattering experiment/02 TIRM/functions.jl
@@ -5,24 +5,39 @@ using DataFrames
 
 # Constants
 # from previous analysis
-# D0=0.122
-# R=1.86
+D0=0.122
+R=1.86
 
 # from matlab script
-R=2.15
-D0=0.11195
+# R=2.15
+# D0=0.11195
 
 Δt=1e-3
-
-# Define functions
-# remove_offset(I) = I .- minimum(vcat(df.I...))
-remove_offset(I) = abs.(I)
-# calculate z
-z_estimate(I; β=1, I0=1) = -log.(remove_offset(I)./I0) .* β
-Dz_theoretical(z; D0=D0, R=R) = D0 ./ (R./z + 0.2 .* log.(R./z) + 0.9712)
 
 # Load data
 cd(@__DIR__)
 df = load("../data/TIRM/data.jld2")["df"]
 df = filter(i -> i.ot<1.1 || i.ot==1.5, df)
 df
+
+# Define functions, depend on df
+mean_slope(y) = mean(diff(y))
+z_estimate(I; β=1, I0=1) = -log.(remove_offset(I)./I0) .* β
+Dz_theoretical(z; D0=D0, R=R) = D0 ./ (R./z + 0.2 .* log.(R./z) + 0.9712)
+
+# remove_offset(I) = I .- minimum(vcat(df.I...))
+# remove_offset(I) = abs.(I)
+"""
+Remove the negative values and the drift from the data
+"""
+function remove_offset(I)
+	I = I .- minimum(I)
+	# remove the drift without changing the mean
+	m = mean(I)
+	I = I .- m
+	slope = mean_slope(I)
+	I = I .- slope .* (1:length(I))
+	I = I .+ m
+	I = abs.(I)
+	return I
+end
