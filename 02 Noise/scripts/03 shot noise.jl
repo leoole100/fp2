431d6fdac3b4b09faf1daee30fa3654dafeb660d
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2024-12-07 14:43:02 +0100

    added shot noise density

--- 02 Noise/scripts/03 shot noise.jl
+++ 02 Noise/scripts/03 shot noise.jl
@@ -67,3 +67,52 @@ ylims!(low=0)
 # axislegend(a, position=:rt)
 save("../figures/03 shot noise.pdf", f)
 f
+
+# %%
+dfs = [
+	DataFrame(CSV.File("../data/04 photocurrent.csv")),
+	DataFrame(CSV.File("../data/05 transimpedance amplifier.csv"))
+]
+
+for d in dfs
+	d.V = measurement.(d.Vsq, load("../data/gen/04 meter uncertainty.jld2")["std"]) 
+	d.V = d.V .* 10 ./ (100 .* d.G2 .*1000).^2 # scale measurements to volts²
+	d.V *= 1/(10000)^2 	# convert to current, with a 10kΩ resistor
+	d.Δf = 1e3.*d.Δf # convert to Hz
+	d.I *= 1e-6 # to A
+	d.S = d.V ./ d.Δf
+	sort!(d, [:I, :Δf])
+end
+
+df = dfs[1]
+
+f = Figure(size=(fullsize[1], halfsize[2]))
+a = Axis(f[1, 1]; 
+	xlabel="Δf in kHz",
+	ylabel="S in (pV)²/Hz",
+	title="Resistor",
+)
+b = Axis(f[1, 2]; 
+	title="Transimpedance amplifier",
+	xlabel="Δf in kHz",
+)
+
+colorrange=(0,
+	value.(maximum([maximum(d.I) for d in dfs]))
+)
+
+for (a, df) in zip([a, b], dfs)
+	for g in groupby(df, :I)
+		stairs!(a, value.(g.Δf)./1e3, value.(g.S)*1e24,
+			color=g.I[1], colorrange=colorrange,
+			step=:pre
+		)
+	end
+end
+Colorbar(f[1, 3], colorrange=colorrange.*1e6, label="I in μA")
+
+linkaxes!(a, b)
+hideydecorations!(b, grid=false)
+save("../figures/06 shot noise density.pdf", f)
+
+f
